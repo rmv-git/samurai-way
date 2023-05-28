0bd07deed629fb2b2f14382763c0a1a1019e13ad
hrsk <hrsk.yegor@gmail.com>
2023-05-28 18:56:45 +0300

    styles sidebar, add NavLink

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {UserType} from "../../types/types";
 import avatar_male from '../../assets/images/avatar_male_person.png';
+import {NavLink} from "react-router-dom";
+import styles from './Sidebar.module.css';
 
 type PropsType = {
     arrayUsers: Array<UserType>;
 }
 export const Sidebar = (props: PropsType) => {
     return (
-        <div style={{display: 'flex', alignItems: 'flex-start', border: 'solid', borderColor: 'white', borderWidth: '2px' }}>
+        <div className={styles.wrapper}>
             {props.arrayUsers.map((user: UserType) => <div key={user.id}>
-                <img src={avatar_male} alt={'avatar_male_images'}
-                     style={{width: '48px', height: '48px'}}/>
-                <div>{user.name}</div>
+                <NavLink to={`/profile/${user.name}`}>
+                    <img className={styles.img}
+                         src={avatar_male}
+                         alt={'avatar_male_images'}/>
+                    <div>{user.name}</div>
+                </NavLink>
             </div>)}
         </div>
     );
