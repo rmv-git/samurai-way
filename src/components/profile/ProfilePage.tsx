d5e0972ab1f360c30dd89707829855cd2903c1e2
hrsk <hrsk.yegor@gmail.com>
2023-06-09 19:00:19 +0300

    profile page class component, connected props, connect component, need do fix

--- src/components/profile/ProfilePage.tsx
+++ src/components/profile/ProfilePage.tsx
@@ -2,19 +2,19 @@ import React, {ChangeEvent} from "react";
 import classes from "./ProfilePage.module.css";
 import {ProfileDescription} from "./description/ProfileDescription";
 import {Posts} from "./posts/Posts";
-import {ProfilePageContainerType} from "./ProfilePageContainer";
+import {ProfilePageContainer, ProfilePageContainerType} from "./ProfilePageContainer";
 
 export const ProfilePage = (props: ProfilePageContainerType) => {
 
-    return (
-        <div className={classes.content}>
-            <ProfileDescription/>
-            <Posts posts={props.arrayPosts}/>
-            <div>
-                <textarea value={props.newPostText}
-                          onChange={(event: ChangeEvent<HTMLTextAreaElement>) => props.updatePost(event.currentTarget.value)}/>
-                <button onClick={() => props.addPost()}>Add</button>
-            </div>
-        </div>
+    return ( <ProfilePageContainer/>
+        // <div className={classes.content}>
+        //     <ProfileDescription/>
+        //     <Posts posts={props.arrayPosts}/>
+        //     <div>
+        //         <textarea value={props.newPostText}
+        //                   onChange={(event: ChangeEvent<HTMLTextAreaElement>) => props.updatePost(event.currentTarget.value)}/>
+        //         <button onClick={() => props.addPost()}>Add</button>
+        //     </div>
+        // </div>
     );
 }
--- src/components/profile/ProfilePageContainer.tsx
+++ src/components/profile/ProfilePageContainer.tsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import {PostType, UserProfileResponseType} from "../../types/types";
 import {ProfilePage} from "./ProfilePage";
-import {connect} from "react-redux";
+import {connect, ConnectedProps} from "react-redux";
 import {RootStateType} from "../../redux/redux-store";
 import {Dispatch} from "redux";
-import {addPostAC, getUserProfileAC, newPostTextAC} from "../../redux/profile-reducer";
+import {addPostAC, getUserProfileAC, getUserProfileThunk, newPostTextAC} from "../../redux/profile-reducer";
+import classes from "./ProfilePage.module.css";
+import {ProfileDescription} from "./description/ProfileDescription";
+import {Posts} from "./posts/Posts";
 
+export class ProfilePageClassComponent extends React.Component<any, any> {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className={classes.content}>
+                <ProfileDescription/>
+                <Posts posts={this.props.arrayPosts}/>
+                <div>
+                <textarea value={this.props.newPostText}
+                          onChange={(event: ChangeEvent<HTMLTextAreaElement>) => this.props.updatePost(event.currentTarget.value)}/>
+                    <button onClick={() => this.props.addPost()}>Add</button>
+                </div>
+            </div>
+        )
+    }
+}
 type MapStateToPropsType = {
     arrayPosts: PostType[];
     newPostText: string;
     profile: UserProfileResponseType;
 }
-type MapDispatchToPropsType = {
-    addPost: () => void;
-    updatePost: (value: string) => void;
-    getUserProfile: (profile: UserProfileResponseType) => void;
-}
-export type ProfilePageContainerType = MapDispatchToPropsType & MapStateToPropsType;
+// type MapDispatchToPropsType = {
+//     addPost: () => void;
+//     updatePost: (value: string) => void;
+//     getUserProfile: (profile: UserProfileResponseType) => void;
+// }
+// export type ProfilePageContainerType = MapDispatchToPropsType & MapStateToPropsType;
 
 const mapStateToProps = (state: RootStateType): MapStateToPropsType => {
     return {
@@ -25,18 +47,26 @@ const mapStateToProps = (state: RootStateType): MapStateToPropsType => {
         profile: state.profileReducer.profile,
     }
 }
-const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
-    return {
-        addPost: () => {
-            dispatch(addPostAC())
-        },
-        updatePost: (value: string) => {
-            dispatch(newPostTextAC(value))
-        },
-        getUserProfile: (profile: UserProfileResponseType) => {
-            dispatch(getUserProfileAC(profile))
-        }
-    }
-}
+// const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
+//     return {
+//         addPost: () => {
+//             dispatch(addPostAC())
+//         },
+//         updatePost: (value: string) => {
+//             dispatch(newPostTextAC(value))
+//         },
+//         getUserProfile: (profile: UserProfileResponseType) => {
+//             dispatch(getUserProfileAC(profile))
+//         }
+//     }
+// }
 
-export const ProfilePageContainer = connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
+export const ConnectComponent = connect(mapStateToProps, {
+    addPostAC,
+    newPostTextAC,
+    getUserProfileThunk,
+});
+
+export type ProfilePageContainerType = ConnectedProps<typeof ConnectComponent>;
+
+export const ProfilePageContainer = ConnectComponent(ProfilePageClassComponent);
